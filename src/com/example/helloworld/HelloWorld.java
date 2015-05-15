d76556f6150939c9719a6976be15ae8f5ec63695
Dende87 <dende76@gmx.de>
2015-05-15 16:34:14 +0200

    Main Java File

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Dende on 15.05.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
